63460456e7e8ff33af41dc0f7bb3439628e89a94
Steven Chen <jeepchenup@foxmail.com>
2019-03-26 20:54:50 +0800

    update

---
+++ src/main/java/info/chen/smallcrud/SmallCRUDApplication.java
@@ -0,0 +1,13 @@
+package info.chen.smallcrud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmallCRUDApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SmallCRUDApplication.class, args);
+    }
+
+}
